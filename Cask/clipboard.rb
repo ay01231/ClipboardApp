3b5fde6393091e3682acdf23864ca2f8f6cd3cb5
ay01231 <ajengayu1105@gmail.com>
2026-03-31 16:21:06 +0700

    add cask

---
+++ Cask/clipboard.rb
@@ -0,0 +1,11 @@
+cask "clipboardapp" do
+  version "1.0.0"
+  sha256 "84069555f5c0596af7527120d01ccf26f2e30fdc3359db6b10318e0464ec6046"
+
+  url "https://github.com/ay01231/ClipboardApp/releases/download/v1.0.0/Clipboard.app.zip"
+  name "ClipboardApp"
+  desc "Simple Clipboard Manager"
+  homepage "https://github.com/ay01231/ClipboardApp"
+
+  app "Clipboard.app"
+end

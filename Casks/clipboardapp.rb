36807c95620d3ed07a27b796a38d334379fe55e9
ay01231 <ajengayu1105@gmail.com>
2026-03-31 19:58:16 +0700

    fix: clipboardapp: fix CLI and add dist files update: readme.md

--- Casks/clipboardapp.rb
+++ Casks/clipboardapp.rb
@@ -1,11 +1,10 @@
 cask "clipboardapp" do
-  version "1.0.0"
-  sha256 "84069555f5c0596af7527120d01ccf26f2e30fdc3359db6b10318e0464ec6046"
-
-  url "https://github.com/ay01231/ClipboardApp/releases/download/v1.0.0/Clipboard.app.zip"
+  version "1.0"
+  sha256 "a4214c97474b1ae03281f445c29ef5908326ec07fd8ab9d13ead2a1c9114b43b"
+  url "https://github.com/ay01231/ClipboardApp/releases/download/v1.0/ClipboardApp.zip"
   name "ClipboardApp"
-  desc "Simple Clipboard Manager"
+  desc "Simple clipboard manager"
   homepage "https://github.com/ay01231/ClipboardApp"
 
-  app "Clipboard.app"
+  app "ClipboardApp.app"
 end

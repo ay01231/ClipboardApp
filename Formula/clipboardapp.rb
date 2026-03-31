da8c26983c4545080b2d4b2cab39508a50763e56
ay01231 <ajengayu1105@gmail.com>
2026-03-31 16:07:20 +0700

    fix: update sha256

--- Formula/clipboardapp.rb
+++ Formula/clipboardapp.rb
@@ -2,7 +2,7 @@ class Clipboardapp < Formula
   desc "Simple clipboard manager for macOS"
   homepage "https://github.com/ay01231/ClipboardApp"
   url "https://github.com/ay01231/ClipboardApp/releases/download/v1.0.0/Clipboard.app.zip"
-  sha256 "de850730a4367d08f6c95b4864b94933d726953781100d38f8f0acdf665d2026"
+  sha256 "ca95f93f4e2aa32a54dd9e44e8fffea420199d7019c1e3935e926fbaa8df583e"
   version "1.0.0"
 
   def install

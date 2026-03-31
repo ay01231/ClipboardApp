b11fe58ce08c9df0c920d1f10d266d45376ce02a
ay01231 <ajengayu1105@gmail.com>
2026-03-31 16:13:45 +0700

    fix: use release binary

--- Formula/clipboardapp.rb
+++ Formula/clipboardapp.rb
@@ -2,7 +2,7 @@ class Clipboardapp < Formula
   desc "Simple clipboard manager for macOS"
   homepage "https://github.com/ay01231/ClipboardApp"
   url "https://github.com/ay01231/ClipboardApp/releases/download/v1.0.0/Clipboard.app.zip"
-  sha256 "ca95f93f4e2aa32a54dd9e44e8fffea420199d7019c1e3935e926fbaa8df583e"
+  sha256 "84069555f5c0596af7527120d01ccf26f2e30fdc3359db6b10318e0464ec6046"
   version "1.0.0"
 
   def install
